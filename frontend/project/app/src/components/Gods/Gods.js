4cc8810a64f3890839a526e7a9bc7567bf088c23
calanchoe <40577368+calanchoe@users.noreply.github.com>
2018-07-28 00:47:43 +0300

    символы богов

--- frontend/project/app/src/components/Gods/Gods.js
+++ frontend/project/app/src/components/Gods/Gods.js
@@ -3,7 +3,15 @@ import React, { Component } from 'react'
 import './gods.css'
 import './bootstrap.min.css'
 import afina from './image/gods sumbols/afina.jpg';
-
+import aid from './image/gods sumbols/aid.png';
+import ares from './image/gods sumbols/ares.png';
+import gera from './image/gods sumbols/gera.jpg';
+import gipnos from './image/gods sumbols/gipnos.jpg';
+import persefona from './image/gods sumbols/persefona.jpg';
+import poseidon from './image/gods sumbols/poseidon.PNG';
+import tanatos from './image/gods sumbols/tanatos.png';
+import xaron from './image/gods sumbols/xaron.png';
+import zevs from './image/gods sumbols/zevs.PNG';
 
 
 class Gods extends Component {
@@ -15,21 +23,48 @@ class Gods extends Component {
                         <div className="row">
                         {/* bloks whith cards about gods */}
                             <div className="col-md-1-5">
-                            {/*< img src={afina} alt="afina" />*/}
-                            1
+                                < img src={zevs} className="img-fluid" alt="zevs" />
+                                Зевс
                             </div>
-                            <div className="col-md-1-5">2</div>
-                            <div className="col-md-1-5">3</div>
-                            <div className="col-md-1-5">4</div>
-                            <div className="col-md-1-5">5</div>
+                            <div className="col-md-1-5">
+                                < img src={gera} className="img-fluid" alt="gera" />
+                                Гера
+                            </div>
+                            <div className="col-md-1-5">
+                                < img src={poseidon} className="img-fluid" alt="poseidon" />
+                                Посейдон
+                            </div>
+                            <div className="col-md-1-5">
+                                < img src={afina} className="img-fluid" alt="afina" />
+                                Афина
+                            </div>
+                            <div className="col-md-1-5">
+                                < img src={gipnos} className="img-fluid" alt="gipnos" />
+                                Гипнос
+                            </div>
                         </div>
                         <div className="row">
                         {/* bloks whith cards about gods */}
-                            <div className="col-lg-1-5">1</div>
-                            <div className="col-lg-1-5">2</div>
-                            <div className="col-lg-1-5">3</div>
-                            <div className="col-lg-1-5">4</div>
-                            <div className="col-lg-1-5">5</div>
+                            <div className="col-lg-1-5">
+                                < img src={aid} className="img-fluid" alt="aid" />
+                                Аид
+                            </div>
+                            <div className="col-lg-1-5">
+                                < img src={persefona} className="img-fluid" alt="persefona" />
+                                Персефона
+                            </div>
+                            <div className="col-lg-1-5">
+                                < img src={xaron} className="img-fluid" alt="xaron" />
+                                Харон
+                            </div>
+                            <div className="col-lg-1-5">
+                                < img src={ares} className="img-fluid" alt="ares" />
+                                Арес
+                            </div>
+                            <div className="col-lg-1-5">
+                                < img src={tanatos} className="img-fluid" alt="tanatos" />
+                                Танатос
+                            </div>
                         </div>
                         
                     </div>
